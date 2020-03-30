4555b8729676aac595154f0848bc812931d65194
AuggieH <hudakjerome@gmail.com>
2020-03-29 22:59:54 -0400

    add tests for module 3

---
+++ test/module-3/App.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import App from '../../src/components/App'
+import { createTiles } from '../../src/misc/utils'
+
+import { shallow } from 'enzyme'
+
+describe('App', () => {
+  const wrapper = shallow(<App />)
+  const instance = wrapper.instance()
+
+  it('Has a handleTileClicked method @crate-handle-tile-clicked', () => {
+    expect(typeof instance.handleTileClicked, 'Did you create the handleTileClicked method on App?')
+      .toEqual('function')
+  })
+
+  it('calls setState @set-the-state', () => {
+    const setStateSpy = jest.spyOn(App.prototype, 'setState')
+    const wrapper = shallow(<App />)
+    const instance = wrapper.instance()
+    instance.handleTileClicked()
+
+    expect(setStateSpy).toHaveBeenCalledWith({ tiles: [], toBeCleared: null})
+  })
+
+  it('sets the selected tile as the previous tile if its null @find-the-selected-tile', () => {
+    const setStateSpy = jest.spyOn(App.prototype, 'setState')
+    const wrapper = shallow(<App />)
+    const instance = wrapper.instance()
+    instance.startGame(10)
+    
+    const tiles = instance.state.tiles
+    instance.handleTileClicked(tiles[5].id, tiles[5].color)
+
+    expect(setStateSpy).toHaveBeenCalledWith({ previousTile: 5, tiles, toBeCleared: null})
+
+  })
+})
