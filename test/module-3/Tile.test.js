0135a975f33e301364b80294bb8f1418e0dbb66a
AuggieH <hudakjerome@gmail.com>
2020-04-02 21:13:26 -0400

    add module 3 test for @tile-on-click

---
+++ test/module-3/Tile.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Tile from '../../src/components/Tile'
+import One from '../../src/assets/svgs/1'
+
+import { shallow } from 'enzyme'
+
+describe('Tile', () => {
+
+  const mockFn = jest.fn()
+
+  it('Attaches handleTileClicked to the Tile @tile-on-click', () => {
+    const wrapper = shallow(<Tile handleTileClicked={mockFn} id={3} color={'#C0FFEE'} svg={One} />)
+    
+    wrapper.simulate('click')
+
+    expect(mockFn).toHaveBeenCalledWith(3, '#C0FFEE')
+  })
+
+})
+
+
