fbcb68b79daae412b784f9ce52b214ee56327f79
AuggieH <hudakjerome@gmail.com>
2020-04-03 22:14:33 -0400

    add module 4 test for @create-a-ref

--- test/module-4/hooks.test.js
+++ test/module-4/hooks.test.js
@@ -3,8 +3,16 @@ import useHover from '../../src/hooks'
 
 describe('Building a custom hook', () => {
 
+  React.useRef = jest.fn()
+  React.useRef.mockReturnValue('ref')
+
   it('useHover @create-use-hover', () => {
     expect(typeof useHover, 'Did you remember to export useHover?').toBe('function')
   })
 
+  it('calls useRef @create-a-ref', () => {
+    useHover()
+
+    expect(React.useRef).toHaveBeenCalled()
+  })
 })
