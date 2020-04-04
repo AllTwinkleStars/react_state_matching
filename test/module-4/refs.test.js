14d05430e721ae292a18036f2aac28f7e52f4c97
AuggieH <hudakjerome@gmail.com>
2020-04-04 19:57:18 -0400

    add better tests for @attach-ref

--- test/module-4/refs.test.js
+++ test/module-4/refs.test.js
@@ -11,14 +11,15 @@ describe('TileSelector', () => {
 
 
   it('attaches the ref to the div @attach-ref', () => {
+    const dd = container.queryByText('99')
 
     expect(container.queryByText('16'), 'Did you return null from the ternary?').toBe(null)
 
-    fireEvent.mouseEnter(dd)
+    if(dd) fireEvent.mouseEnter(dd)
 
     expect(container.queryByText('16'), 'Did you attach the ref to the right div?').toBeTruthy()
 
-    fireEvent.mouseLeave(dd)
+    if(dd) fireEvent.mouseLeave(dd)
 
     expect(container.queryByText('16'), 'Did you return null from the ternary?').toBe(null)
     
