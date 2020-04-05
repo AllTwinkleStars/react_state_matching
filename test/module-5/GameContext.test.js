71cbeff5f12b000fff90232f367cb552c06310d5
AuggieH <hudakjerome@gmail.com>
2020-04-05 14:13:46 -0400

    add module 5 test for @set-default-context

--- test/module-5/GameContext.test.js
+++ test/module-5/GameContext.test.js
@@ -4,8 +4,18 @@ describe('Game Context', () => {
   React.createContext = jest.fn()
   const GameContext = require('../../refactor/GameContext')
  
-  it('passes the correct arguments to React.Context @create-game-context', () => {
+  it('Creates the GameContext @create-game-context', () => {
 
     expect(React.createContext).toHaveBeenCalled()
+  })
+
+  it('Sets default context values @set-default-context', () => {
+    
+    expect(React.createContext).toHaveBeenCalledWith({
+      numTiles: 36,
+      playing: false,
+      handleNumTileChange: expect.any(Function),
+      startPlaying: expect.any(Function)
+    })
   })
 })
